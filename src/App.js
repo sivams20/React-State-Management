8d5b7db5a0e9f22ca553f4b9f4a214ac0b3a1ca9
unknown <sivams20@gmail.com>
2022-06-02 09:24:04 +0530

    Redux store with connect and mapStatesToProps, mapDispatchtToProps

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
-import CakeContainer from './components/cakeContainer';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import CakeContainer from './components/cakeContainer';
+import HooksCakeContainer from './components/HooksCakeContainer';
+
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <h2>Using Connect</h2>
         <CakeContainer />
+        <h2>Using Hooks</h2>
+        <HooksCakeContainer/>
       </div>
     </Provider>
   );
