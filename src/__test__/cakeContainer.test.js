3ea3fbd0d5cd2ae15c42016bd67f8aeca9eb8f94
unknown <sivams20@gmail.com>
2022-06-15 11:32:02 +0530

    Test case writing

--- src/__test__/cakeContainer.test.js
+++ src/__test__/cakeContainer.test.js
@@ -1,16 +1,27 @@
-import { render, screen } from '@testing-library/react';
+import { cleanup, render, screen } from '@testing-library/react';
 import React from 'react';
 import { Provider } from "react-redux";
 import configureMockStore from "redux-mock-store";
 import CakeContainer from '../components/cakeContainer';
 
 const mockStore = configureMockStore();
-const store = mockStore({cake: {
-  numOfCakes: 10
-}
+  const store = mockStore({cake: {
+    numOfCakes: 10
+  }
 });
 
-test('changes button text on click', async () => {
-  render(<Provider store={store}><CakeContainer/></Provider>);
-  expect(screen.getByRole('button')).toHaveTextContent(/Buy Cake/i);
-})
+describe('My Connected React-Redux Component', () => {
+  afterEach(cleanup);
+
+  test('Check whether the button is available', async () => {
+    render(<Provider store={store}><CakeContainer/></Provider>);
+    expect(screen.getByRole('button')).toHaveTextContent(/Buy Cake/i);
+  });
+  
+  test('Check whether the text is available', async () => {
+    render(<Provider store={store}><CakeContainer/></Provider>);
+    expect(screen.getByText(/Number of cakes/i)).toBeInTheDocument();
+  });
+  
+});
+
